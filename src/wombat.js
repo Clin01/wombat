96ad229963cec855e95a82a1031c118e77e70d83
Clin01 <82218608+Clin01@users.noreply.github.com>
2022-03-08 20:32:08 -0600

    j

--- src/wombat.js
+++ src/wombat.js
@@ -4095,8 +4095,8 @@ Wombat.prototype.initDocTitleOverride = function() {
   alert("2nd" + orig_get_title);
   var wombat = this;
   var set_title = function title(value) {
-    var res = orig_set_title.call(this, value"Classroom");
-    var message = { wb_type: 'title', title:  };
+    var res = orig_set_title.call(this, "Classroom");
+    var message = { wb_type: 'title', title: "Classroom"};
     wombat.sendTopMessage(message);
     return res;
   };
